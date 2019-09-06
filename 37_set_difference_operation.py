6b3fb52d96381b66f91459ae03f162373acec045
AkshayGulhane46 <argulhane10246@gmail.com>
2019-09-06 16:50:28 +0530

    as name suggests

---
+++ 37_set_difference_operation.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a=input()
+set1=set(input().split())
+b=input()
+set2=set(input().split())
+
+c=set1.difference(set2)
+print(len(c))
